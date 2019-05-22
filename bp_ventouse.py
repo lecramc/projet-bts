318d6b2befe589ebb72a3208c039859a2b5e081d
Clement MARCEL <clementmarcel@outlook.fr>
2019-05-22 19:08:34 +0200

    program bp test1

--- bp_ventouse.py
+++ bp_ventouse.py
@@ -1,21 +1,4 @@
-import RPi.GPIO as GPIO  # Gestion des GPIO
-from time import sleep   # Gestion du temps
-
-GPIO.setmode(GPIO.BCM)   # La numerotation choisie
-GPIO.setup(21, GPIO.IN) # Une sortie : la poussoir
-
-def my_callback(channel):
-  if GPIO.input(channel):
-    print('GPIO %s 0->1' %channel)
-    GPIO.output(21, GPIO.LOW)
-  else:
-    print('GPIO %s 1->0' %channel)
-    GPIO.output(21, GPIO.HIGH)
-
-print("Vous pouvez aussi terminer avec CTRL+C \n")
-
-GPIO.add_event_detect(16, GPIO.BOTH, callback=my_callback)
-print("Maintenant, le programme surveille les actions sur le poussoir\n")
-
-while True:
-  sleep(30)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+GPIO.output(21, GPIO.HIGH)
